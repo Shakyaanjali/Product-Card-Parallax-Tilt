ae6b3f843aab3da03338820123899736f4156e39
Anjali <97797520+Shakyaanjali@users.noreply.github.com>
2024-05-16 18:59:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var modeSwitch = document.querySelector('.mode-switch');
+  
+    modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+      modeSwitch.classList.toggle('active');
+    });
+  });
